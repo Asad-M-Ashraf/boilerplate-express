73143f93405684152a32add12364569e4b887550
Asad-M-Ashraf <connectasadm@gmail.com>
2024-05-29 01:54:33 +0500

    Up Until Env

--- myApp.js
+++ myApp.js
@@ -1,14 +1,33 @@
 let express = require('express');
+let path = require("path");
 let app = express();
+require("dotenv").config();
 
 console.log("Hello World!")
 
+// app.get("/", function(req, res){
+//     res.send("Hello Express");
+// });
+
+app.get("/", (req, res) => {
+    // const indexPath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use("/public", express.static(__dirname + "/public"));
 
 
-
-
-
-
+app.get("/json", (req, res) => {
+    let message = "Hello json";
+    if(process.env["MESSAGE_STYLE"] === 'uppercase'){
+        message = message.toUpperCase();
+    } else {
+        message = "Hello json";
+    }
+    res.json({
+        "message": message
+    })
+});
 
 
 
