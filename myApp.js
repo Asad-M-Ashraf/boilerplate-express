b0310328614357610f853accc80844289550c1cf
Asad-M-Ashraf <connectasadm@gmail.com>
2024-05-29 17:24:51 +0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -9,13 +9,18 @@ console.log("Hello World!")
 //     res.send("Hello Express");
 // });
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} ${req.ip}`);
+    next();
+});
+
+
 app.get("/", (req, res) => {
     // const indexPath = path.join(__dirname, 'views', 'index.html');
     res.sendFile(__dirname + "/views/index.html");
 });
-
-app.use("/public", express.static(__dirname + "/public"));
-
 
 app.get("/json", (req, res) => {
     let message = "Hello json";
